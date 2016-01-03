2b4aadd6432570601839723a523f7e974ad0024c
yangzhuo <563102472@qq.com>
2016-01-03 21:41:57 +0800

    sf

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yangzhuo on 16-1-3.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+    }
+}
